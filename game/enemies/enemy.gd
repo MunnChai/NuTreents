5497a470f5f79caed7ce92887b8cdf888fce1d01
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-03-19 03:53:00 -0700

    Fixed enemy pathfinding, added animations

--- game/enemies/enemy.gd
+++ game/enemies/enemy.gd
@@ -58,9 +58,16 @@ func move_along_path() -> bool:
 	var terrain_map: TerrainMap = get_tree().get_first_node_in_group("terrain_map")
 	var global_pos: Vector2 = terrain_map.map_to_local(next_tile)
 	
-	var tween: Tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	tween.tween_property(self, "position", global_pos, MOVE_DURATION)
+	var pos_tween: Tween = get_tree().create_tween()
+	pos_tween.set_ease(Tween.EASE_IN_OUT)
+	pos_tween.set_trans(Tween.TRANS_CUBIC)
+	pos_tween.tween_property(self, "position", global_pos, MOVE_DURATION)
+	
+	var offset_tween: Tween = get_tree().create_tween()
+	offset_tween.set_ease(Tween.EASE_IN_OUT)
+	offset_tween.set_trans(Tween.TRANS_CUBIC)
+	offset_tween.tween_property(sprite_2d, "position:y", -20, MOVE_DURATION / 2)
+	offset_tween.tween_property(sprite_2d, "position:y", -16, MOVE_DURATION / 2)
 	
 	return true
 
@@ -85,12 +92,13 @@ func find_path_to_tree(tree: Twee) -> Array:
 	
 	var to_visit: Array[Vector2i] = [map_position]
 	var paths: Array[Array] = [[]] # Nested typed collections aren't supported 😔
+	var visited: Array[Vector2i] = []
 	
 	var current_pos: Vector2i
 	var current_path: Array
 	while (not to_visit.is_empty()):
 		# Get "best" tile to visit next
-		var index_of_best: int = get_lowest_f_score(to_visit, paths, Vector2(target_pos)) 
+		var index_of_best: int = get_lowest_f_score(to_visit, paths, target_pos) 
 		
 		current_pos = to_visit[index_of_best]
 		current_path = paths[index_of_best]
@@ -98,20 +106,26 @@ func find_path_to_tree(tree: Twee) -> Array:
 		to_visit.erase(current_pos)
 		paths.erase(current_path)
 		
+		if (visited.has(current_pos)):
+			continue
+		
+		visited.append(current_pos)
+		
 		# We reached the target!
 		if (get_taxicab_distance(current_pos, target_pos) <= attack_range):
+			print("Total tiles visited during enemy pathfinding: ", visited.size())
 			return current_path
 		
 		# Check if 4 adjacent tiles are valid
 		# If so, add them to to_visit and paths
 		for next_pos: Vector2i in get_adjacent_tiles(current_pos):
-			if (is_valid_tile(next_pos)):
+			if (!visited.has(next_pos) && is_valid_tile(next_pos)): 
 				to_visit.append(next_pos)
-				var new_path = current_path.duplicate()
+				var new_path = current_path.duplicate(true)
 				new_path.append(next_pos)
 				paths.append(new_path)
 	
-	return []
+	return [] # could not find path
 
 
 # Munn: If different enemies can traverse different tiles, then this should be changed.
@@ -119,8 +133,13 @@ func is_valid_tile(map_pos: Vector2i) -> bool:
 	
 	var terrain_map: TerrainMap = get_tree().get_first_node_in_group("terrain_map")
 	
+	# Do not go on non-existent tiles
+	var tile_data: TileData = terrain_map.get_cell_tile_data(map_pos)
+	if (tile_data == null):
+		return false
+	
 	# Do not go on Water
-	var biome: int = terrain_map.get_cell_tile_data(map_pos).get_custom_data("biome")
+	var biome: int = tile_data.get_custom_data("biome")
 	if (biome == 3): # WARNING: HARD CODED, DOES NOT USE ENUMS!
 		return false
 	
@@ -132,18 +151,18 @@ func is_valid_tile(map_pos: Vector2i) -> bool:
 	return true
 
 # Returns the index of the tile with the lowest F score (f = dist_from_start + approx_dist_to_end)
-func get_lowest_f_score(to_visit: Array[Vector2i], paths: Array[Array], target_pos: Vector2) -> int:
+func get_lowest_f_score(to_visit: Array[Vector2i], paths: Array[Array], target_pos: Vector2i) -> int:
 	
 	var lowest_f_score: float = INF;
 	var best_index: int = -1
 	
-	for i in range(0, to_visit.size() - 1):
-		var g_score: int = paths[i].size();
+	for i in range(0, paths.size() - 1):
+		var g_score: int = paths[i].size(); # dist_from_start
 		
-		var map_pos = to_visit[i]
-		var h_score: float = (Vector2(map_pos) - target_pos).length();
+		var map_pos = paths[i].back()
+		var h_score: float = Vector2(map_pos - target_pos).length(); # approx_dist_to_end
 		
-		var f_score = g_score + h_score
+		var f_score = g_score + (h_score * 2) # Value approx_dist_to_end more than dist_from_start
 		
 		if (f_score < lowest_f_score):
 			lowest_f_score = f_score
@@ -181,3 +200,11 @@ func get_nearest_tree() -> Twee:
 
 func get_taxicab_distance(pos_i: Vector2i, pos_j: Vector2i) -> int:
 	return abs(pos_i.x - pos_j.x) + abs(pos_i.y - pos_j.y)
+
+func array_has(array: Array[Vector2i], what: Vector2i) -> bool:
+	
+	for thing: Vector2i in array:
+		if (is_zero_approx(thing.distance_to(what))):
+			return true
+	
+	return false
