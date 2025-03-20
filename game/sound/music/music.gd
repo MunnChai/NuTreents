e8d723556945dacd76f753ac3a8e010831679e47
felix liu <felix.liu.ray@gmail.com>
2025-03-19 17:02:06 -0700

    Add rudimentary adaptive music

---
+++ game/sound/music/music.gd
@@ -0,0 +1,37 @@
+extends Node
+
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+
+var clock: Clock
+
+func _ready() -> void:
+	clock = get_tree().root.find_child("Clock", true, false)
+
+
+enum Vibe {
+	SMALL_FOREST,
+	MEDIUM_FOREST,
+	LARGE_FOREST,
+	CALM_NIGHT,
+	MILD_NIGHT,
+	INTENSE_NIGHT,
+	SILENCE
+}
+
+var current_vibe = Vibe.CALM_NIGHT
+
+func _process(delta: float) -> void:
+	if len(get_tree().get_nodes_in_group("enemies")) > 15:
+		if current_vibe != Vibe.INTENSE_NIGHT:
+			audio_stream_player.get_stream_playback().switch_to_clip_by_name("intense")
+			current_vibe = Vibe.INTENSE_NIGHT
+		# audio_stream_player["parameters/switch_to_clip"] = "intense"
+	elif len(get_tree().get_nodes_in_group("enemies")) > 3:
+		if current_vibe != Vibe.MILD_NIGHT:
+			audio_stream_player.get_stream_playback().switch_to_clip_by_name("mild")
+			current_vibe = Vibe.MILD_NIGHT
+			# audio_stream_player["parameters/switch_to_clip"] = "mild"
+	else:
+		if current_vibe != Vibe.CALM_NIGHT:
+			audio_stream_player.get_stream_playback().switch_to_clip_by_name("calm")
+			current_vibe = Vibe.CALM_NIGHT
