bc74a81d7dbf087853174cedd3c9d6cc7883008d
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-03-22 16:15:37 -0700

    Fixed card

--- game/ui/menus/tree_menu/tree_card.gd
+++ game/ui/menus/tree_menu/tree_card.gd
@@ -22,16 +22,16 @@ extends Control
 func _ready() -> void:
 	var net_water = tree_stat.gain.y - tree_stat.maint
 	if (net_water > 0):
-		water_plus.text = str(net_water)
-		water_min.text = str(0)
+		water_plus.text = str(int(net_water))
+		water_min.text = str(int(0))
 	else:
-		water_plus.text = str(0)
-		water_min.text = str(abs(net_water))
+		water_plus.text = str(int(0))
+		water_min.text = str(int(abs(net_water)))
 	
-	sun_plus.text = str(tree_stat.gain.z)
-	nutrients_plus.text = str(tree_stat.gain.x)
-	sun_min.text = str(0)
-	nutrients_min.text = str(0)
+	sun_plus.text = str(int(tree_stat.gain.z))
+	nutrients_plus.text = str(int(tree_stat.gain.x))
+	sun_min.text = str(int(0))
+	nutrients_min.text = str(int(0))
 	species.text = tree_stat.name
 	cost.text = "Cost: " + str(tree_stat.cost_to_purchase)
 
