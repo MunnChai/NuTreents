3cc38ee399706cdf85eb141b478dcae8d224072a
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-03-21 17:29:01 -0700

    Add clock to globals

--- game/global.gd
+++ game/global.gd
@@ -4,7 +4,9 @@ const MAP_SIZE: Vector2i = Vector2i(1, 1) * 40
 
 var structure_map: BuildingMap
 var terrain_map: TerrainMap
+var clock: Clock
 
 func _ready() -> void:
 	structure_map = get_tree().get_first_node_in_group("structure_map")
 	terrain_map = get_tree().get_first_node_in_group("terrain_map")
+	clock = get_tree().get_first_node_in_group("clock")
