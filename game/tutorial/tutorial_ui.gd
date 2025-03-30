f30de029ab7c5238da9a55de6ba264ced8fbe1c5
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-03-29 22:40:25 -0700

    Changed tutorial to only allow input sometimes

--- game/tutorial/tutorial_ui.gd
+++ game/tutorial/tutorial_ui.gd
@@ -2,10 +2,11 @@ extends Control
 
 const ANIMATION_LIBRARY_NAME: String = "tutorial_animation"
 
-# Various references to parts of the UI
+# Various references to random parts of the game
 @onready var tree_menu_button = $"../Menu/MenuButtons/Trees"
 @onready var menu = $"../Menu"
-
+@onready var cursor = $"../../../World/TileMaps/BuildingManager/Cursor"
+@onready var input_handler = $"../../../World/InputHandler"
 
 # Animation stuff
 @onready var animation_player = $AnimationPlayer
@@ -21,6 +22,9 @@ func _ready() -> void:
 	
 	# Connect 1 time signals
 	connect_tutorial_signals()
+	
+	cursor.is_visible = false
+	input_handler.is_enabled = false
 
 # Plays the next animation in the list
 func play_next_animation() -> void:
@@ -30,6 +34,7 @@ func play_next_animation() -> void:
 func play_animation(index: int) -> void:
 	print(ANIMATION_LIBRARY_NAME + "/" + get_animation_name(index))
 	animation_player.play(ANIMATION_LIBRARY_NAME + "/" + str(get_animation_name(index)))
+	print(current_animation)
 
 func get_animation_name(index: int) -> StringName:
 	var list = animation_library.get_animation_list()
@@ -44,6 +49,13 @@ func get_animation_name(index: int) -> StringName:
 		if (list_name.begins_with(str(index))):
 			animation_name = list_name
 	
+	if (index == 5 || index >= 7):
+		cursor.is_visible = true
+		input_handler.is_enabled = true
+	else:
+		cursor.is_visible = false
+		input_handler.is_enabled = false
+	
 	return animation_name
 
 
@@ -54,10 +66,6 @@ func connect_tutorial_signals() -> void:
 	
 	# Connect place a tree
 	TreeManager.tree_placed.connect(_on_first_tree_placed)
-	
-	# Connect
-	
-	pass
 
 const TREE_MENU_ANIMATION = 4
 func _on_tree_menu_opened():
@@ -84,7 +92,7 @@ func skip_tutorial() -> void:
 const END_TUTORIAL_INDEX = 9
 func _on_continue_button_pressed():
 	SfxManager.play_sound_effect("ui_click")
-
+	
 	if (current_animation == TREE_MENU_ANIMATION && menu.is_open):
 		menu.close_menu()
 	
