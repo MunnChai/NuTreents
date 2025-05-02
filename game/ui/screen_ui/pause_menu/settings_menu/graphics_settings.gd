9b3de7242c88eb9f2d1af8f7f0a3f58e6d1cccdc
felix liu <felix.liu.ray@gmail.com>
2025-05-02 13:55:10 -0700

    Re-enable GUI scale customization

--- game/ui/screen_ui/pause_menu/settings_menu/graphics_settings.gd
+++ game/ui/screen_ui/pause_menu/settings_menu/graphics_settings.gd
@@ -2,8 +2,9 @@ class_name GraphicsSettings
 extends PanelContainer
 
 ## BUG: GUI Scale causes issues with a lot of parts of the game...
-## Like controller cursor, camera movement, etc.
-## Camera is not updated when the GUI scale is changed while paused...
+## - After switching, camera may be weird (not updated) until unpaused
+## - Camera movement via controller is bugged because limits change
+## - Main menu has bad anchors/offsetting
 
 @export var window_option_button: OptionButton
 @export var resolution_option_button: OptionButton
@@ -27,7 +28,7 @@ func load_graphics_settings() -> void:
 	gui_scale_slider.value = Settings.get_setting_or_default("gui_scale", 1.0)
 	window_option_button.selected = Settings.get_setting_or_default("window_option", 0)
 	
-	#get_tree().root.content_scale_factor = gui_scale_slider.value
+	get_tree().root.content_scale_factor = gui_scale_slider.value
 	#get_tree().root.content_scale_factor = 1.2
 
 func _on_window_option_button_item_selected(index: int) -> void:
@@ -47,7 +48,7 @@ func _on_window_option_button_item_selected(index: int) -> void:
 
 
 func _on_gui_scale_slider_drag_ended(value_changed: bool) -> void:
-	#get_tree().root.content_scale_factor = gui_scale_slider.value
+	get_tree().root.content_scale_factor = gui_scale_slider.value
 	save_graphics_settings()
 
 func _on_gui_scale_slider_value_changed(value: float) -> void:
