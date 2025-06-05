8c898a5730c0d86ba553539f2124c9c505ae9a03
felix liu <felix.liu.ray@gmail.com>
2025-06-04 19:53:14 -0700

    Refactor keybind menu code

--- game/ui/screen_ui/pause_menu/settings_menu/keybinds_menu/keybinds_menu.gd
+++ game/ui/screen_ui/pause_menu/settings_menu/keybinds_menu/keybinds_menu.gd
@@ -2,9 +2,12 @@ class_name KeybindsMenu
 extends ScreenMenu
 
 ## TODO
-## - Log changes, and ask for confirmation upon exiting...
+## - Keep a copy of all keybinds JUST AS the menu is opened.
+##   Then, on exit, ask for confirmation. 
+##   If confirmation declined, reset all keybinds to JUST BEFORE menu was opened.
+## - Gamepad/controller keybinds in a second tab.
 
-## SCREEN MENU IMPLEMENTATION
+#region SCREEN MENU IMPLEMENTATION
 
 @onready var starting_position := position
 
@@ -34,41 +37,57 @@ func return_to(previous_menu: ScreenMenu) -> void:
 	## ANIMATION
 	open(previous_menu)
 
-## KEYBIND REMAPPING
+#endregion
 
-static var instance: KeybindsMenu
-var is_rebinding := false
+#region KEYBINDING MENU
+
+static var instance: KeybindsMenu # Psuedo-singleton for quick access...
+var is_rebinding := false # Are we currently rebinding something?
+
+# Used to track change from not rebinding to rebinding and vice versa
+var previously_was_rebinding := false 
 
 func _ready() -> void:
 	instance = self
 	is_rebinding = false
 
-var just_rebinded := false
 func _process(delta: float) -> void:
 	if is_rebinding:
-		if not just_rebinded:
-			ScreenUI.keybinding_dimmer.show()
-		just_rebinded = true
-		%BackButton.disabled = true
+		if not previously_was_rebinding:
+			switch_on_rebinding_mode()
+		previously_was_rebinding = true
 	else:
-		if just_rebinded:
-			ScreenUI.keybinding_dimmer.hide()
-		just_rebinded = false
-		%BackButton.disabled = false
+		if previously_was_rebinding:
+			switch_off_rebinding_mode()
+		previously_was_rebinding = false
+
+func switch_on_rebinding_mode() -> void:
+	ScreenUI.keybinding_dimmer.show()
+	%BackButton.disabled = true
+	
+	TweenUtil.pop_delta(%TabContainer, Vector2(0.15, 0.15), 0.2)
+
+func switch_off_rebinding_mode() -> void:
+	ScreenUI.keybinding_dimmer.hide()
+	%BackButton.disabled = false
+	
+	TweenUtil.pop_delta(%TabContainer, Vector2(0.15, 0.15), 0.2)
+
 
 func _on_back_button_pressed() -> void:
 	SfxManager.play_sound_effect("ui_click")
 	ScreenUI.exit_menu()
 
-func _on_reset_choice(choice: bool) -> void:
-	if choice:
-		for child in %KeybindContainer.get_children():
-			if child is KeybindLine:
-				child.reset_value()
-				SfxManager.play_sound_effect("ui_pages")
-	else:
-		SfxManager.play_sound_effect("ui_click")
+#endregion
 
+#region RESET TO DEFAULTS
+
+func _on_reset_button_pressed() -> void:
+	SfxManager.play_sound_effect("ui_click")
+	#TweenUtil.pop_delta(%TabContainer, Vector2(0.15, 0.15), 0.2)
+	await _prompt_for_reset()
+
+## Generate confirmation of reset prompt... and link to callback
 func _prompt_for_reset() -> void:
 	ScreenUI.add_menu(ScreenUI.confirmation_menu)
 	ScreenUI.confirmation_menu.set_message("Are you sure you want to reset all keybinds to defaults?")
@@ -77,6 +96,16 @@ func _prompt_for_reset() -> void:
 	ScreenUI.confirmation_menu.connect_choice_to(_on_reset_choice)
 	await ScreenUI.confirmation_menu.choice_made
 
-func _on_reset_button_pressed() -> void:
-	SfxManager.play_sound_effect("ui_click")
-	await _prompt_for_reset()
+## Confirmation of reset prompt callback
+func _on_reset_choice(choice: bool) -> void:
+	if choice:
+		# Get all individual KeybindLines
+		for child in %KeybindContainer.get_children():
+			if child is KeybindLine:
+				child.reset_value()
+				SfxManager.play_sound_effect("ui_pages")
+		TweenUtil.pop_delta(%TabContainer, Vector2(0.15, 0.15), 0.2)
+	else:
+		SfxManager.play_sound_effect("ui_click")
+
+#endregion
