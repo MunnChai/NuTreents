0f854af4afbab8f31300fb9607c2d7facb3ce45d
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-06-13 14:39:28 -0700

    Change mother tree hitbox, fix silk spitter animation

--- game/enemies/enemy_manager.gd
+++ game/enemies/enemy_manager.gd
@@ -27,10 +27,10 @@ func _input(event: InputEvent) -> void:
 	if (Global.game_state != Global.GameState.PLAYING):
 		return
 	
-	if (Input.is_action_just_pressed("debug_button")):
-		var terrain_map = get_tree().get_first_node_in_group("terrain_map")
-		var map_coord = terrain_map.local_to_map(terrain_map.get_local_mouse_position()) # one HELL of a line
-		spawn_enemy(Global.EnemyType.SPEEDLE, map_coord)
+	#if (Input.is_action_just_pressed("debug_button")):
+		#var terrain_map = get_tree().get_first_node_in_group("terrain_map")
+		#var map_coord = terrain_map.local_to_map(terrain_map.get_local_mouse_position()) # one HELL of a line
+		#spawn_enemy(Global.EnemyType.SPEEDLE, map_coord)
 
 func _process(delta: float) -> void:
 	if (Global.game_state != Global.GameState.PLAYING):
