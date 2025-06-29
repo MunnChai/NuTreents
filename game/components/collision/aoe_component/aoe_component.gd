cf7883d64afeeefc6bbaebb068da37a17a9c925c
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-06-29 10:24:31 -0700

    Slight refactor and updated enemy stats

--- game/components/collision/aoe_component/aoe_component.gd
+++ game/components/collision/aoe_component/aoe_component.gd
@@ -2,7 +2,7 @@ class_name AoeComponent
 extends HitboxComponent
 
 @export var explode_range: float
-var grid_range_component: GridRangeComponent
+@export var explosion_range_component: GridRangeComponent
 var grid_pos_component: GridPositionComponent
 
 var actor: Node2D
@@ -10,12 +10,12 @@ var range: Array[Vector2i]
 
 func _ready() -> void:
 	actor = get_parent()
-	if not grid_range_component:
-		grid_range_component = Components.get_component(actor, GridRangeComponent)
+	if not explosion_range_component:
+		explosion_range_component = Components.get_component(actor, GridRangeComponent)
 	if not grid_pos_component:
 		grid_pos_component = Components.get_component(actor, GridPositionComponent)
-	grid_range_component.range = explode_range
-	range = grid_range_component.get_tiles_in_range()
+	
+	range = explosion_range_component.get_tiles_in_range()
 	range.erase(Vector2i(0,0))
 
 func explode(pos: Vector2i) -> void:
