7475f29b072540fd5fe11d0f34a4af861f197af3
felix liu <felix.liu.ray@gmail.com>
2025-07-06 20:01:58 -0700

    Test having listener on cursor instead of camera

--- game/sound/sound_player.gd
+++ game/sound/sound_player.gd
@@ -13,5 +13,5 @@ func _process(delta: float) -> void:
 func update_attributes() -> void:
 	if Camera.instance:
 		var zoom_level := Camera.instance.get_percent_zoom()
-		attenuation = lerp(base_attenuation, base_attenuation * 2.0, zoom_level)
+		attenuation = lerp(base_attenuation / 4.0, base_attenuation * 2.0, zoom_level)
 		volume_linear = lerp(base_volume * 0.5, base_volume, zoom_level)
