66357bef6002f795084c278a3f1de5df40dce477
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-07-10 14:37:33 -0700

    Fixed tutorial missing tree card container

--- game/enemies/enemy_manager.gd
+++ game/enemies/enemy_manager.gd
@@ -33,11 +33,11 @@ func _input(event: InputEvent) -> void:
 	if (Global.game_state != Global.GameState.PLAYING):
 		return
 	
-	if (Input.is_action_just_pressed("debug_button")):
-		var terrain_map = get_tree().get_first_node_in_group("terrain_map")
-		var map_coord = terrain_map.local_to_map(terrain_map.get_local_mouse_position()) # one HELL of a line
+	#if (Input.is_action_just_pressed("debug_button")):
+		#var terrain_map = get_tree().get_first_node_in_group("terrain_map")
+		#var map_coord = terrain_map.local_to_map(terrain_map.get_local_mouse_position()) # one HELL of a line
 		#spawn_enemy(Global.EnemyType.values().pick_random(), map_coord)
-		spawn_enemy(Global.EnemyType.BOMB_BUG, map_coord)
+		#spawn_enemy(Global.EnemyType.BOMB_BUG, map_coord)
 
 func _process(delta: float) -> void:
 	if (Global.game_state != Global.GameState.PLAYING):
