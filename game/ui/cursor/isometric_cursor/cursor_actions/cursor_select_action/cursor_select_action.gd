ead88feea9b6c53670736019e30c909a1c7eec40
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-07-10 15:18:11 -0700

    Added guards to primary action

--- game/ui/cursor/isometric_cursor/cursor_actions/cursor_select_action/cursor_select_action.gd
+++ game/ui/cursor/isometric_cursor/cursor_actions/cursor_select_action/cursor_select_action.gd
@@ -5,15 +5,33 @@ extends CursorAction
 func execute_primary_action(cursor: IsometricCursor) -> void:
 	var iso_position: Vector2i = cursor.iso_position
 	
+	## EARLY TERMINATIONS
+	var flag = cursor.get_hover_flag(iso_position)
+	match flag:
+		IsometricCursor.HoverFlag.VOID:
+			return
+		IsometricCursor.HoverFlag.OBSCURED:
+			return
+		_:
+			pass
+	
 	var entity: Node2D = MapUtility.get_entity_at(iso_position)
-	if entity:
-		if cursor.get_selected_entity() != entity:
-			cursor.set_selected_entity(entity)
-			SoundManager.play_oneshot(&"ui_click", Global.structure_map.map_to_local(iso_position))
-		else:
-			cursor.set_selected_entity(null)
-	else:
+	if not entity:
 		cursor.set_selected_entity(null)
+		return
+	
+	if cursor.get_selected_entity() == entity:
+		cursor.set_selected_entity(null)
+		return
+	
+	var tooltip: TooltipIdentifierComponent = Components.get_component(entity, TooltipIdentifierComponent)
+	if not tooltip:
+		cursor.set_selected_entity(null)
+		return
+	
+	cursor.set_selected_entity(entity)
+	SoundManager.play_oneshot(&"ui_click", Global.structure_map.map_to_local(iso_position))
+
 
 func execute_secondary_action(cursor: IsometricCursor) -> void:
 	cursor.set_selected_entity(null)
